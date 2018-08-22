1f8865ae3116246dcf4bee2eff26374306af872a
Adam Beswick <adam.beswick@fluxfederation.com>
2018-08-22 14:37:13 +1200

    removed accessors from the foot traffic class. remove viewer include from foot traffic class

--- lib/foot_traffic.rb
+++ lib/foot_traffic.rb
@@ -1,15 +1,13 @@
 require_relative './climate'
 
 class FootTraffic
-  include Viewer
-
   attr_accessor :walkers
 
   def initialize
   end
 
   def walker_forecast(weather)
-    @walkers = 10  unless @walkers != nil
-    @walkers *= weather
+    walkers = 10  unless walkers != nil
+    walkers *= weather
   end
 end
